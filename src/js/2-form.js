e2a2aa70039f7279177cd9ac1b491d901fa9b6cd
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-03-26 21:17:15 +0100

    Update 2-forms

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,30 @@
 const form = document.querySelector(".feedback-form");
-const textarea = form.querySelector("textarea");
-const input = form.querySelector("input");
 
+let formState;
+getFormInput();
 form.addEventListener("submit", handelSubmit);
+form.addEventListener("input", onFormInput);
 
 function handelSubmit(event) {
     event.preventDefault;
     const email = event.target.elements.email.value;
     const message = event.target.elements.message.value;
-    console.log(email);
-    console.log(message);
-    
     if (email && message) {
         console.log({ email, message });
         localStorage.removeItem("feedback-form-state");
         event.currentTarget.reset();
 }
+}
+function onFormInput(event) {
+     formState[event.target.name] = event.target.value.trim();
+     localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+   
+}
+function getFormInput() {
+   formState = JSON.parse(localStorage.getItem("feedback-form-state")) || {};
+    let savedEmail = formState.email;
+    let savedMessage = formState.message;
+    if (savedEmail) {form.elements.email.value = savedEmail }; 
+    if (savedMessage) { form.elements.message.value = savedMessage }; 
+     
 }
