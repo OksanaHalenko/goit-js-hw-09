d2f13b225b31315190f5f3d1d62279c30e4f6d99
OksanaHalenko <150601280+OksanaHalenko@users.noreply.github.com>
2024-03-26 15:22:48 +0100

    Update gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,3 +1,6 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css"
+
 const images = [
   {
     preview:
@@ -80,7 +83,11 @@ function createGallery(arr) {
         .join("");
 }
 gallery.insertAdjacentHTML("beforeend", createGallery(images));
-import SimpleLightbox from "simplelightbox";
-const lightboxGallery = new SimpleLightbox('.gallery a');
-lightboxGallery.open();
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionsPosition: "bottom",
+    captionsDelay: 250
+});
+
 
